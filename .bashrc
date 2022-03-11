ad6cd382f15e40b4c06350dd98c7fc492d8cbae0
maru-256 <maru043@docomo.ne.jp>
2022-03-11 18:35:08 +0900

    update install.sh

--- .bashrc
+++ .bashrc
@@ -1,22 +1,10 @@
-export TERM=xterm-256color
-export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-export PATH=$PATH:/usr/local/go/bin
-export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:$HOME/go/bin
-. "$HOME/.cargo/env"
-MYVIMRC="$HOME/.vimrc"
-export PATH=$PATH:/opt/gradle/gradle-7.2/bin
-
 if [[ -t 0 ]]; then
     stty stop undef
     stty start undef
 fi
-
-export DENO_INSTALL=$HOME/.deno
-export PATH=$PATH:$DENO_INSTALL/bin
+. "$HOME/.cargo/env"
--- install.sh
+++ install.sh
@@ -2,3 +2,22 @@
 
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+
+curl -L https://go.dev/dl/go1.17.8.linux-amd64.tar.gz -o go.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tar.gz
+rm -rf go.tar.gz
+
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bash_profile
+echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bash_profile
+echo "export PATH=\$PATH:\$HOME/go/bin" >> ~/.bash_profile
+
+curl https://sh.rustup.rs -sSf -o /tmp/rustup.rs | sh /tmp/rustup.rs -y
+echo "source \$HOME/.cargo/env"
+
+echo "export MYVIMRC=\$HOME/.vimrc" >> ~/.bash_profile
+
+curl -fsSL https://deno.land/install.sh | sh
+echo "export DENO_INSTALL=\$HOME/.deno" >> ~/.bash_profile
+echo "export PATH=\$PATH:\$DENO_INSTALL/bin" >> ~/.bash_profile
