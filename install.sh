8d2a987a4bbaf6cb16a3024dc5481f2848b67fc8
maru-256 <maru043@docomo.ne.jp>
2022-03-11 21:51:24 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+rm ~/.vimrc
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm ~/.bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+
+rm ~/.bash_profile
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
 echo "export MYVIMRC=\$HOME/.vimrc" >> ~/.bash_profile
