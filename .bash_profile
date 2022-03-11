38bd4d9f6ea330899e2763788321bc76248c9d0f
maru-256 <maru043@docomo.ne.jp>
2022-03-11 23:51:31 +0900

    update move envs to .bash_profile

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,15 @@
 export TERM=xterm-256color
 export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
+export MYVIMRC=\$HOME/.vimrc
+
+export DENO_INSTALL=$HOME/.deno
+export PATH=$PATH:$DENO_INSTALL/bin
+
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:$HOME/go/bin
+
+source $HOME/.cargo/env
+
 source ~/.bashrc
--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 rm ~/.bash_profile
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
-echo "export MYVIMRC=\$HOME/.vimrc" >> ~/.bash_profile
-
 sudo apt install git
 sudo apt install build-essential
 
@@ -22,16 +20,9 @@ rm -rf go.tar.gz
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/x-motemen/ghq@latest
 
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bash_profile
-echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bash_profile
-echo "export PATH=\$PATH:\$HOME/go/bin" >> ~/.bash_profile
-
 curl https://sh.rustup.rs -sSf -o /tmp/rustup.rs | sh /tmp/rustup.rs -y
-echo "source \$HOME/.cargo/env"
 
 curl -fsSL https://deno.land/install.sh | sh
-echo "export DENO_INSTALL=\$HOME/.deno" >> ~/.bash_profile
-echo "export PATH=\$PATH:\$DENO_INSTALL/bin" >> ~/.bash_profile
 
 sudo apt install tmux
 
