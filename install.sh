7b726e1677c2075d1f6fa76c600eb23ebff40505
maru-256 <maru043@docomo.ne.jp>
2022-03-13 03:20:10 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 rm ~/.bash_profile
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
-sudo apt install git
-sudo apt install build-essential
+source ~/.bash_profile
+
+sudo apt install build-essential curl git
 
 curl -L https://go.dev/dl/go1.17.8.linux-amd64.tar.gz -o go.tar.gz
 sudo rm -rf /usr/local/go
@@ -20,7 +21,8 @@ rm -rf go.tar.gz
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/x-motemen/ghq@latest
 
-curl https://sh.rustup.rs -sSf -o /tmp/rustup.rs | sh /tmp/rustup.rs -y
+curl https://sh.rustup.rs -sSf -o /tmp/rustup.rs 
+sh /tmp/rustup.rs -y
 
 curl -fsSL https://deno.land/install.sh | sh
 
@@ -37,5 +39,4 @@ cd ~/ghq/github.com/vim/vim/src
 git checkout v8.2.4503
 ~/dotfiles/build-vim.sh && make && sudo make install && cd -
 
-source ~/.bashrc
 source ~/.bash_profile
